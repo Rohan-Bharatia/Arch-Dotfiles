c79eece472ceb095fb794a9796bcb38a650dda9d
Rohan Bharatia <Rohan-Bharatia@github.com>
2025-08-18 18:25:51 -0400

    Fix dependency list

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cd $HOME
+
 sudo pacman -Syu
-sudo pacman -S kitty hyprland hyprpaper hyprlock hypridle hyprshot waybar wofi neofetch pulseaudio playerctl gtk3
-yay -S swaync zen-browser nerd-fonts-complete
+sudo pacman -S kitty hyprland hyprpaper hyprlock hypridle hyprshot waybar wofi pipewire playerctl gtk3 git starship
+
+if ! command -v yay >/dev/null; then
+    mkdir -p $HOME/tmp
+    cd $HOME/tmp
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd $HOME
+fi
+
+yay -S swaync nerd-fonts neofetch zen-browser
